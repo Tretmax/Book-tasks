dbf88a6c8454c20ea4e71d809dae5e40d6038c6a
Tretmax <1tretmax1@gmail.com>
2022-05-05 23:08:14 +0300

    init

---
+++ alert.js
@@ -0,0 +1,13 @@
+// Task1
+// alert ('i am JS!')
+
+// Task2
+//let admin
+//let name = "John"
+//admin = name
+//alert ( admin)
+
+//Task3
+// let name = prompt("What is your name?")
+// alert (`Your name is ${name}`)
+// alert ("Your name is " +name)
